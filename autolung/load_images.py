3741bcdf693dc76f598e929a834c809e3ca79fc2
jcalendo <jcalendo@gmail.com>
2019-05-31 14:56:24 -0400

    replaced os.listdir with pathlib

--- autolung/load_images.py
+++ autolung/load_images.py
@@ -2,11 +2,13 @@
 
 (c) 2019 Gennaro Calendo, Laboratory of Marla R. Wolfson, MS, PhD at Lewis Katz School of Medicine at Temple University
 """
-import os
+from pathlib import Path
 
 
 def collect(input_dir):
     """Iterate through image directory collecting image paths for processing
+
+    This method currently only accepts .tif images
     
     Arguments:
         input_dir {str} -- path to the directory containing images
@@ -15,16 +17,7 @@ def collect(input_dir):
         list -- list of image paths to process
     """
 
-    # create empty list for images that wil be processed
-    to_process = []
+    p = Path(input_dir)
+    image_files = [f.absolute() for f in p.glob('**/*.tif') if f.is_file()]
 
-    # define allowed extensions
-    ext = [".tif"]
-
-    for file in os.listdir(input_dir):
-        if file.endswith(tuple(ext)):
-            to_process.append(os.path.join(input_dir, file))
-        else:
-            print("{} is not compatible for image processing. Must be .tif".format(file))
-
-    return to_process
+    return image_files

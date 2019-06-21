0aeb750a933ffa80ced71721967c369c0578bdce
jcalendo <jcalendo@gmail.com>
2019-06-21 10:48:48 -0400

    added validate_settings function

--- autolung/load_config.py
+++ autolung/load_config.py
@@ -5,6 +5,29 @@
 import configparser
 
 
+def validate_settings(**kwargs):
+    """Validate settings from configuration file
+
+    Arguments:
+        **kwargs -- settings read from config file or their defaults from .get
+    
+    Returns:
+        dict -- validated settings
+    """
+    # check if block_size is odd
+    if kwargs['block_size'] % 2 == 0:
+        print(f"ERROR: Invalid Block_Size '{kwargs['block_size']}' -- must be odd integer, check your config file")
+        print("Setting Block_Size to 251")
+        kwargs['block_size'] = 251
+
+    if kwargs['method'] not in ('mean', 'median', 'gaussian'):
+        print(f"ERROR: Invalid Method '{kwargs['method']}' -- must be one of 'mean', 'median', or 'gaussian', check your config file")
+        print("Setting Method to 'mean'")
+        kwargs['method'] = 'mean'
+
+    return kwargs
+
+
 def load_settings(config_file):
     """Read contents of config file and pass on as settings for image processing
     
@@ -29,19 +52,11 @@ def load_settings(config_file):
     magnification = metadata.get('Magnification', '10X')
     fixed_field = metadata.get('Fixed_Field', '2560x1920')
     scale = float(metadata.get('Scale', 2.0969))
-    block_size = int(threshold_params.get('Block_Size', 641))
+    block_size = int(threshold_params.get('Block_Size', 251))
     constant = int(threshold_params.get('Constant', 0))
     method = str(threshold_params.get('Method', 'mean'))
     min_alv_size = int(morphometry_params.get('Min_Alveolar_Size', 500))
     max_speckle_size = int(morphometry_params.get('Max_Speckle_Size', 100))
-
-    if method not in ('mean', 'median', 'gaussian'):
-        print("ERROR: Unrecognized 'Method', defaulting to 'mean' - check your config file")
-        method = 'mean'
-    
-    if block_size % 2 == 0:
-        print("ERROR: Block_Size is not an odd integer, defaulting to 641 -- check your config file")
-        block_size = 641
 
     settings = {"species" : species,
                 "magnification" : magnification,
@@ -54,4 +69,6 @@ def load_settings(config_file):
                 "max_speckle_size" : max_speckle_size
                 }
 
-    return settings
+    validated = validate_settings(**settings)
+
+    return validated
